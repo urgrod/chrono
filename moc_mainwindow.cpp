303b4cacfb2d70e7b9647594acba299a97d5ee72
urgrod <lecam.gurvan@gmail.com>
2018-07-27 15:35:21 +0200

    2d marche2d marche

--- moc_mainwindow.cpp
+++ moc_mainwindow.cpp
@@ -19,8 +19,8 @@
 
 QT_BEGIN_MOC_NAMESPACE
 struct qt_meta_stringdata_MainWindow_t {
-    QByteArrayData data[3];
-    char stringdata0[30];
+    QByteArrayData data[14];
+    char stringdata0[196];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -31,10 +31,26 @@ static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
     {
 QT_MOC_LITERAL(0, 0, 10), // "MainWindow"
 QT_MOC_LITERAL(1, 11, 17), // "showDirectoryMenu"
-QT_MOC_LITERAL(2, 29, 0) // ""
+QT_MOC_LITERAL(2, 29, 0), // ""
+QT_MOC_LITERAL(3, 30, 19), // "showManualInputMenu"
+QT_MOC_LITERAL(4, 50, 13), // "populateArray"
+QT_MOC_LITERAL(5, 64, 19), // "selectColorBestTime"
+QT_MOC_LITERAL(6, 84, 22), // "selectColorAverageTime"
+QT_MOC_LITERAL(7, 107, 20), // "selectColorIdealTime"
+QT_MOC_LITERAL(8, 128, 18), // "selectColorOutTime"
+QT_MOC_LITERAL(9, 147, 12), // "saveSettings"
+QT_MOC_LITERAL(10, 160, 12), // "loadSettings"
+QT_MOC_LITERAL(11, 173, 14), // "openEditWindow"
+QT_MOC_LITERAL(12, 188, 3), // "row"
+QT_MOC_LITERAL(13, 192, 3) // "col"
 
     },
-    "MainWindow\0showDirectoryMenu\0"
+    "MainWindow\0showDirectoryMenu\0\0"
+    "showManualInputMenu\0populateArray\0"
+    "selectColorBestTime\0selectColorAverageTime\0"
+    "selectColorIdealTime\0selectColorOutTime\0"
+    "saveSettings\0loadSettings\0openEditWindow\0"
+    "row\0col"
 };
 #undef QT_MOC_LITERAL
 
@@ -44,7 +60,7 @@ static const uint qt_meta_data_MainWindow[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-       1,   14, // methods
+      10,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -52,10 +68,28 @@ static const uint qt_meta_data_MainWindow[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   19,    2, 0x0a /* Public */,
+       1,    0,   64,    2, 0x0a /* Public */,
+       3,    0,   65,    2, 0x0a /* Public */,
+       4,    0,   66,    2, 0x0a /* Public */,
+       5,    0,   67,    2, 0x0a /* Public */,
+       6,    0,   68,    2, 0x0a /* Public */,
+       7,    0,   69,    2, 0x0a /* Public */,
+       8,    0,   70,    2, 0x0a /* Public */,
+       9,    0,   71,    2, 0x0a /* Public */,
+      10,    0,   72,    2, 0x0a /* Public */,
+      11,    2,   73,    2, 0x0a /* Public */,
 
  // slots: parameters
     QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int, QMetaType::Int,   12,   13,
 
        0        // eod
 };
@@ -67,10 +101,18 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         Q_UNUSED(_t)
         switch (_id) {
         case 0: _t->showDirectoryMenu(); break;
+        case 1: _t->showManualInputMenu(); break;
+        case 2: _t->populateArray(); break;
+        case 3: _t->selectColorBestTime(); break;
+        case 4: _t->selectColorAverageTime(); break;
+        case 5: _t->selectColorIdealTime(); break;
+        case 6: _t->selectColorOutTime(); break;
+        case 7: _t->saveSettings(); break;
+        case 8: _t->loadSettings(); break;
+        case 9: _t->openEditWindow((*reinterpret_cast< int(*)>(_a[1])),(*reinterpret_cast< int(*)>(_a[2]))); break;
         default: ;
         }
     }
-    Q_UNUSED(_a);
 }
 
 const QMetaObject MainWindow::staticMetaObject = {
@@ -98,13 +140,13 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 1)
+        if (_id < 10)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 1;
+        _id -= 10;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 1)
+        if (_id < 10)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 1;
+        _id -= 10;
     }
     return _id;
 }
